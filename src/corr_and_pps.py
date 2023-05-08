de64ba0a017d833124d452b3101f1bae80c2b981
Matthew Murnaghan <matt.murnaghan@outlook.ie>
2023-05-08 12:22:03 +0100

    Move corr and pps methods to ext module

---
+++ src/corr_and_pps.py
@@ -0,0 +1,114 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import ppscore as pps
+import streamlit as st
+
+
+def heatmap_corr(df, threshold, figsize=(20, 12), font_annot=8):
+    if len(df.columns) > 1:
+        mask = np.zeros_like(df, dtype=np.bool)
+        mask[np.triu_indices_from(mask)] = True
+        mask[abs(df) < threshold] = True
+
+        fig, axes = plt.subplots(figsize=figsize)
+        sns.heatmap(df, annot=True, xticklabels=True, yticklabels=True,
+                    mask=mask, cmap='viridis', annot_kws={"size": font_annot}, ax=axes,
+                    linewidth=0.5
+                    )
+        axes.set_yticklabels(df.columns, rotation=0)
+        plt.ylim(len(df.columns), 0)
+        plt.show()
+
+def heatmap_corr_st(df, threshold, figsize=(20, 12), font_annot=8):
+    if len(df.columns) > 1:
+        mask = np.zeros_like(df, dtype=np.bool)
+        mask[np.triu_indices_from(mask)] = True
+        mask[abs(df) < threshold] = True
+
+        fig, axes = plt.subplots(figsize=figsize)
+        sns.heatmap(df, annot=True, xticklabels=True, yticklabels=True,
+                    mask=mask, cmap='viridis', annot_kws={"size": font_annot}, ax=axes,
+                    linewidth=0.5
+                    )
+        axes.set_yticklabels(df.columns, rotation=0)
+        plt.ylim(len(df.columns), 0)
+        st.pyplot(fig)
+
+
+def heatmap_pps(df, threshold, figsize=(20, 12), font_annot=8):
+    if len(df.columns) > 1:
+        mask = np.zeros_like(df, dtype=np.bool)
+        mask[abs(df) < threshold] = True
+        fig, ax = plt.subplots(figsize=figsize)
+        ax = sns.heatmap(df, annot=True, xticklabels=True, yticklabels=True,
+                         mask=mask, cmap='rocket_r', annot_kws={"size": font_annot},
+                         linewidth=0.05, linecolor='grey')
+        plt.ylim(len(df.columns), 0)
+        plt.show()
+
+def heatmap_pps_st(df, threshold, figsize=(20, 12), font_annot=8):
+    if len(df.columns) > 1:
+        mask = np.zeros_like(df, dtype=np.bool)
+        mask[abs(df) < threshold] = True
+        fig, ax = plt.subplots(figsize=figsize)
+        ax = sns.heatmap(df, annot=True, xticklabels=True, yticklabels=True,
+                         mask=mask, cmap='rocket_r', annot_kws={"size": font_annot},
+                         linewidth=0.05, linecolor='grey')
+        plt.ylim(len(df.columns), 0)
+        st.pyplot(fig)
+
+
+def CalculateCorrAndPPS(df):
+    df_corr_spearman = df.corr(method="spearman")
+    df_corr_pearson = df.corr(method="pearson")
+
+    pps_matrix_raw = pps.matrix(df)
+    pps_matrix = pps_matrix_raw.filter(['x', 'y', 'ppscore']).pivot(columns='x', index='y', values='ppscore')
+
+    pps_score_stats = pps_matrix_raw.query("ppscore < 1").filter(['ppscore']).describe().T
+    print("PPS threshold - check PPS score IQR to decide threshold for heatmap \n")
+    print(pps_score_stats.round(3))
+
+    return df_corr_pearson, df_corr_spearman, pps_matrix
+
+
+def DisplayCorrAndPPS(df_corr_pearson, df_corr_spearman, pps_matrix, CorrThreshold, PPS_Threshold,
+                      figsize=(20, 12), font_annot=8, streamlit=None):
+
+    print("\n")
+    print("* Analyse how the target variable for your ML models are correlated with other variables (features and target)")
+    print("* Analyse multi-colinearity, that is, how the features are correlated among themselves")
+
+    print("\n")
+    print("*** Heatmap: Spearman Correlation ***")
+    print("It evaluates monotonic relationship \n")
+    if not streamlit:
+        heatmap_corr(df=df_corr_spearman, threshold=CorrThreshold, figsize=figsize, font_annot=font_annot)
+    else:
+        heatmap_corr_st(df=df_corr_spearman, threshold=CorrThreshold, figsize=figsize, font_annot=font_annot)
+
+    print("\n")
+    print("*** Heatmap: Pearson Correlation ***")
+    print("It evaluates the linear relationship between two continuous variables \n")
+    if not streamlit:
+        heatmap_corr(df=df_corr_pearson, threshold=CorrThreshold, figsize=figsize, font_annot=font_annot)
+    else:
+        heatmap_corr_st(df=df_corr_pearson, threshold=CorrThreshold, figsize=figsize, font_annot=font_annot)
+
+    print("\n")
+    print("*** Heatmap: Power Predictive Score (PPS) ***")
+    print(f"PPS detects linear or non-linear relationships between two columns.\n"
+          f"The score ranges from 0 (no predictive power) to 1 (perfect predictive power) \n")
+    if not streamlit:
+        heatmap_pps(df=pps_matrix, threshold=PPS_Threshold, figsize=figsize, font_annot=font_annot)
+    else:
+        heatmap_pps_st(df=pps_matrix, threshold=PPS_Threshold, figsize=figsize, font_annot=font_annot)
+
+def plot_corr_pearson_pps_st(df):
+    df_corr_pearson, df_corr_spearman, pps_matrix = CalculateCorrAndPPS(df)
+    DisplayCorrAndPPS(df_corr_pearson = df_corr_pearson,
+                        df_corr_spearman = df_corr_spearman, 
+                        pps_matrix = pps_matrix,
+                        CorrThreshold = 0.6, PPS_Threshold =0.5,
+                        figsize=(12*1,10*1), font_annot=10, streamlit=True) 

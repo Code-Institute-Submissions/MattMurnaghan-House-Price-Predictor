be5a20accbe64bf2e1dfa24c21eea8c5cae734a8
Matthew Murnaghan <matt.murnaghan@outlook.ie>
2023-05-08 12:24:37 +0100

    Add method to generate corr and pps heatmaps

--- app_pages/price_study.py
+++ app_pages/price_study.py
@@ -1,6 +1,7 @@
 import streamlit as st
 
 from src.manage_files import load_housing_price_data
+from src.corr_and_pps import plot_corr_pearson_pps_st
 
 import matplotlib.pyplot as plt
 import seaborn as sns
@@ -13,13 +14,13 @@ def price_study_body():
     df = load_housing_price_data()
 
     # 7 variables that correlate to Sale Price, uncovered in the Variable Study notebook
-    corr_var_list = {'1stFlrSF',
+    corr_vars = ['1stFlrSF',
     'GarageArea',
     'GarageYrBlt',
     'GrLivArea',
     'OverallQual',
     'TotalBsmtSF',
-    'YearBuilt'}
+    'YearBuilt']
 
     st.write("### House Sale Price Study")
     st.info(
@@ -30,7 +31,7 @@ def price_study_body():
         )
 
     # inspect data
-    if st.checkbox("Inspect House Price Data:"):
+    if st.checkbox("Inspect house price data:"):
         st.write(
             f"* The dataset has {df.shape[0]} rows and {df.shape[1]} columns, "
             "The first 5 rows are displayed below.")
@@ -48,6 +49,9 @@ def price_study_body():
         "**1stFlrSF, GarageArea, GrLivArea, GarageYrBlt, OverallQual, "
         "TotalBsmtSF, YearBuilt**"
     )
+
+    if st.checkbox("Inspect Spearman and Pearson Correlation"):
+        plot_corr_pearson_pps_st(df)
 
     # Text based on "sale price study" notebook
     # "Conclusions and Next steps" section
@@ -69,18 +73,18 @@ def price_study_body():
 
     # Code copied from "sale price study" notebook
     # "EDA on the Correlated Variable List" section
-    df_eda = df.filter(corr_var_list + ['SalePrice'])
+    df_eda = df.filter(corr_vars + ['SalePrice'])
 
     # Individual plots per variable
     if st.checkbox("Variable correlation to Sale Price"):
-        variable_correlation_to_sale_price(df_eda, corr_var_list)
+        variable_correlation_to_sale_price(df_eda, corr_vars)
 
 
-def variable_correlation_to_sale_price(df_eda, corr_var_list):
+def variable_correlation_to_sale_price(df_eda, corr_vars):
     # function created using "sale price study" notebook
     # "Visualize variable correlation to Sale Price" section
     target_var = 'SalePrice'
-    for col in corr_var_list:
+    for col in corr_vars:
         plot_numerical(df_eda, col, target_var)
         st.write("\n\n")
 
@@ -93,3 +97,4 @@ def plot_numerical(df, col, target_var):
     sns.regplot(data=df, x=col, y=target_var)
     plt.title(f"{col}", fontsize=20)
     st.pyplot(fig)  # st.pyplot() renders image, in notebook is plt.show()
+
